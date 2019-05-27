cfdaf1d23136c92d0512925b2ab7de3775674464
xmenji <35322102+xmenji@users.noreply.github.com>
2019-05-26 21:04:34 -0700

    Create  TargetHeartRateCalc.java

---
+++ TargetHeartRateCalc.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author xmenji
+ */
+public class TargetHeartRateCalc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
