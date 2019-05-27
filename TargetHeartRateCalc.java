3a483db69dfac4a2fd9034b76bb9628803b9ace4
xmenji <35322102+xmenji@users.noreply.github.com>
2019-05-27 12:49:27 -0700

    Update TargetHeartRateCalc.java

--- TargetHeartRateCalc.java
+++ TargetHeartRateCalc.java
@@ -1,8 +1,10 @@
+
+package targetheartratecalc;
 import java.util.Scanner;
 
 /**
  *
- * @author xmenji
+ * @author Titan
  */
 public class TargetHeartRateCalc {
 
@@ -10,7 +12,35 @@ public class TargetHeartRateCalc {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        String firstName, 
+                lastName,
+                birthMonth;
+        
+        int
+                birthDay,
+                birthYear,
+                age,
+                maxHeartRate;
+        
+        
+        System.out.print("Enter first name: ");
+        firstName = input.nextLine();
+        System.out.print("Enter last name: ");
+        lastName = input.nextLine();
+        System.out.print("Enter month of birth: ");
+        birthMonth = input.nextLine();
+        System.out.print("Enter day of birth: ");
+        birthDay = input.nextInt();
+        System.out.print("Enter year of birth: ");
+        birthYear = input.nextInt();
+        
+        HeartRates person1 = new HeartRates(firstName, lastName, birthMonth, birthDay, birthYear);
+        
+        age = person1.getAge();
+        
+        System.out.printf("%s's Maximum Heart Rate: %d%n", person1.getFirstName(), person1.getMaxHeartRate(age));
+        System.out.println(person1.getTargetHeartRate(age));
     }
     
 }
